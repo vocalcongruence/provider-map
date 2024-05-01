3a06eb65983d4733b2eabdeee9697801592415e6
Ryan Kustaborder <rkusty23@gmail.com>
2024-05-01 19:28:58 -0400

    base for building query

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 // Initialize and add the map
 let map;
 
@@ -11,7 +9,6 @@ let markers = [];
 
 const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 const { Map } = await google.maps.importLibrary("maps");
-
 
 async function initMap() {
   map = new Map(document.getElementById("map"), {
@@ -36,14 +33,11 @@ function buildQuery() {
   let state;
   if (country == "US") {
     state = $("#opt-state").val();
-  }
-  else if (country == "CA") {
+  } else if (country == "CA") {
     state = $("#opt-province").val();
-  }
-  else {
+  } else {
     state = null;
   }
-  
 
   return {
     isTrainer: isTrainer,
@@ -120,44 +114,117 @@ loadProviders();
 */
 
 function attachEvents() {
-  $('#button-openLeftPanel').on('click', showLeftPanel);
-  $('#button-closeLeftPanel').on('click', hideLeftPanel);
-  $('#button-tabFilter').on('click', openTabFilter);
-  $('#button-tabList').on('click', openTabList);
+  $("#button-openLeftPanel").on("click", showLeftPanel);
+  $("#button-closeLeftPanel").on("click", hideLeftPanel);
+  $("#button-tabFilter").on("click", openTabFilter);
+  $("#button-tabList").on("click", openTabList);
+
+  $("#query").on("click", providerQuery);
 }
 
 function showLeftPanel() {
-  $('#panel-left').removeClass("hidden");
+  $("#panel-left").removeClass("hidden");
 }
 
 function hideLeftPanel() {
-  $('#panel-left').addClass("hidden");
+  $("#panel-left").addClass("hidden");
 }
 
-function showRightPanel() {
+function showRightPanel() {}
 
-}
-
-function hideRightPanel() {
-
-}
+function hideRightPanel() {}
 
 function openTabFilter() {
   showLeftPanel();
-  $('#panel-left-tabList').addClass("hidden");
-  $('#panel-left-tabFilter').removeClass("hidden");
+  $("#panel-left-tabList").addClass("hidden");
+  $("#panel-left-tabFilter").removeClass("hidden");
 }
 
 function openTabList() {
   showLeftPanel();
-  $('#panel-left-tabFilter').addClass("hidden");
-  $('#panel-left-tabList').removeClass("hidden");
+  $("#panel-left-tabFilter").addClass("hidden");
+  $("#panel-left-tabList").removeClass("hidden");
 }
 
+function providerQuery() {
+  // Profession
+  const profession = $("#opt-profession").val();
 
+  // Professional Area
+  const pa_slp = $("#opt-professionalArea-slp").is(":checked");
+  const pa_vpst = $("#opt-professionalArea-vpst").is(":checked");
+  const pa_tac = $("#opt-professionalArea-tac").is(":checked");
+  const pa_gavt = $("#opt-professionalArea-gavt").is(":checked");
+  const pa_other = $("#opt-professionalArea-other").is(":checked");
 
+  // Location
+  const country = $("#opt-country").val();
 
+  // State
+  const state = $("#opt-state").val();
 
+  // Language
+  const lang_english = $("#opt-language-english").is(":checked");
+  const lang_spanish = $("#opt-language-spanish").is(":checked");
+  const lang_french = $("#opt-language-french").is(":checked");
+  const lang_mandarin = $("#opt-language-mandarin").is(":checked");
 
+  // Modality
+  const modality = $('input[name="opt-modality"]:checked').val();
+
+  // Number
+  const number = $('input[name="opt-number"]:checked').val();
+
+  // Goals
+  const goal_masculine = $("#opt-goal-masculine").is(":checked");
+  const goal_feminine = $("#opt-goal-feminine").is(":checked");
+  const goal_androgynous = $("#opt-goal-androgynous").is(":checked");
+  const goal_singing = $("#opt-goal-singing").is(":checked");
+
+  // Provider Identity
+  const pi_cisman = $("#opt-pi-cisman").is(":checked");
+  const pi_ciswoman = $("#opt-pi-ciswoman").is(":checked");
+  const pi_transman = $("#opt-pi-transman").is(":checked");
+  const pi_transwoman = $("#opt-pi-transwoman").is(":checked");
+  const pi_nonbinary = $("#opt-pi-nonbinary").is(":checked");
+  const pi_other = $("#opt-pi-other").is(":checked");
+
+  const q = {
+    profession: profession,
+    professionalArea: {
+      slp: pa_slp,
+      vpst: pa_vpst,
+      tac: pa_tac,
+      gavt: pa_gavt,
+      other: pa_other,
+    },
+    country: country,
+    state: state,
+    language: {
+      english: lang_english,
+      spanish: lang_spanish,
+      french: lang_french,
+      mandarin: lang_mandarin,
+    },
+    modality: modality,
+    number: number,
+    goal: {
+      masculine: goal_masculine,
+      feminine: goal_feminine,
+      androgynous: goal_androgynous,
+      singing: goal_singing,
+    },
+    identity: {
+      cisman: pi_cisman,
+      ciswoman: pi_ciswoman,
+      transman: pi_transman,
+      transwoman: pi_transwoman,
+      nonbinary: pi_nonbinary,
+      other: pi_other,
+    },
+  };
+  console.log(q);
+  return q;
+}
 
 attachEvents();
