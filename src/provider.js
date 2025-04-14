8d462f770c7f57b51cbf653c3a01ea15a831fe52
vocalcongruence <167366403+vocalcongruence@users.noreply.github.com>
2025-04-13 22:33:22 -0400

    Update provider.js

--- src/provider.js
+++ src/provider.js
@@ -194,7 +194,11 @@ function buildProviderListItem(map, trainers, surgeons, languages, provider) {
 
     // In Person
     if (nm.individual_inPerson || nm.group_inPerson) {
-      let str = provider.city + " " + provider.state + ", " + provider.country;
+      let cityText = provider.city != null ? provider.city : "";
+      let stateText = provider.city != null ? provider.state : "";
+      let countryText = provider.city != null ? provider.country : "";
+    
+      let str = cityText + " " + stateText + ", " + countryText;
 
       const inPerson = document.createElement("p");
       inPerson.className = "inPerson-container";
